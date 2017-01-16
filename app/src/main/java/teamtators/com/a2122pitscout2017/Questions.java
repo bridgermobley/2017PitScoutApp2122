3d6a39edc2a452177058fd1e46f30364f48fbb05
bridgermobley <bridgermobley@gmail.com>
2017-01-15 18:28:22 -0700

    Initial Commit

---
+++ app/src/main/java/teamtators/com/a2122pitscout2017/Questions.java
@@ -0,0 +1,13 @@
+package teamtators.com.a2122pitscout2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Questions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+    }
+}
