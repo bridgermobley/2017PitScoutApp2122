76c037e0b0e46366c7ae45df434f4912e9b9d131
bridgermobley <bridgermobley@gmail.com>
2017-02-02 20:12:46 -0700

    Added Driver Questions

--- app/src/main/java/teamtators/com/a2122pitscout2017/Questions.java
+++ app/src/main/java/teamtators/com/a2122pitscout2017/Questions.java
@@ -1,13 +1,29 @@
 package teamtators.com.a2122pitscout2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class Questions extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+            }
+
+
     }
+
+
+
+
+
 }
