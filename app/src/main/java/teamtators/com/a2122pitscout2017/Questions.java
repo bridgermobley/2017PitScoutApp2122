4f83a3784adf1499ef4898b8dc359f3d48849cb5
bridgermobley <bridgermobley@gmail.com>
2017-02-02 20:26:00 -0700

    Fixed a layout error

--- app/src/main/java/teamtators/com/a2122pitscout2017/Questions.java
+++ app/src/main/java/teamtators/com/a2122pitscout2017/Questions.java
@@ -1,10 +1,7 @@
 package teamtators.com.a2122pitscout2017;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 
 
 public class Questions extends AppCompatActivity {
@@ -25,5 +22,3 @@ public class Questions extends AppCompatActivity {
 
 
 
-
-}
