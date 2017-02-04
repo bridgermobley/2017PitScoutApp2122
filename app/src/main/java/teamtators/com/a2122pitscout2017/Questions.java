538cd9276d7d0970ca0a53489f03790ae153f80e
bridgermobley <bridgermobley@gmail.com>
2017-02-03 22:58:19 -0700

    Added OnClickListener to change activity

--- app/src/main/java/teamtators/com/a2122pitscout2017/Questions.java
+++ app/src/main/java/teamtators/com/a2122pitscout2017/Questions.java
@@ -1,8 +1,10 @@
 package teamtators.com.a2122pitscout2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 public class Questions extends AppCompatActivity {
 
@@ -14,6 +16,21 @@ public class Questions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+
+        Button buttonNext = (Button) findViewById(R.id.buttonNext);
+
+        buttonNext.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View View)  {
+                        startActivity (new Intent(Questions.this, Comments.class));
+
+                    }
+                }
+
+
+        );
+
+
             }
 
 
