177c87d7562dfe0653663efe0c0a8ae740dcd502
dalo2012 <daviddalo2012@gmail.com>
2017-02-09 20:22:53 -0700

    Set up Spinner

--- app/src/main/java/teamtators/com/a2122pitscout2017/Comments.java
+++ app/src/main/java/teamtators/com/a2122pitscout2017/Comments.java
@@ -1,13 +1,37 @@
 package teamtators.com.a2122pitscout2017;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class Comments extends AppCompatActivity {
+
+    CheckBox cbxCrossLine, cbxDelayAuto, cbxPlaceGear, cbxShootFuel, cbxHopper, cbxPickBalls, cbxStartKey, cbxStartNextKey, cbxStartMiddle, cbxStartLoad;
+    RadioButton rad1Year, rad2Year, rad3Year, rad4Year, rad5Year;
+    EditText txtComments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
+        cbxCrossLine=(CheckBox)findViewById(R.id.checkCross);
+        cbxDelayAuto=(CheckBox)findViewById(R.id.checkDelay);
+        cbxPlaceGear=(CheckBox)findViewById(R.id.checkGear);
+        cbxShootFuel=(CheckBox)findViewById(R.id.checkShoot);
+        cbxHopper=(CheckBox)findViewById(R.id.checkHopper);
+        cbxPickBalls=(CheckBox)findViewById(R.id.checkPick);
+        cbxStartKey=(CheckBox)findViewById(R.id.radioStartInKey);
+        cbxStartNextKey=(CheckBox)findViewById(R.id.radioStartNextKey);
+        cbxStartMiddle=(CheckBox)findViewById(R.id.radioStartMiddle);
+        cbxStartLoad=(CheckBox)findViewById(R.id.radioStartLoadingStation);
+        rad1Year=(RadioButton)findViewById(R.id.radio1Year);
+        rad2Year=(RadioButton)findViewById(R.id.radio2Year);
+        rad3Year=(RadioButton)findViewById(R.id.radio3Year);
+        rad4Year=(RadioButton)findViewById(R.id.radio4Year);
+        rad5Year=(RadioButton)findViewById(R.id.radio5Year);
+        txtComments=(EditText)findViewById(R.id.txtComments);
+        
     }
 }
--- app/src/main/java/teamtators/com/a2122pitscout2017/Questions.java
+++ app/src/main/java/teamtators/com/a2122pitscout2017/Questions.java
@@ -1,24 +1,43 @@
 package teamtators.com.a2122pitscout2017;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Questions extends AppCompatActivity {
 
     EditText scoutName, robotWeight, robotHeight, robotWidth, robotLength, numWheels, typeWheels, ballCap, startLoc, autoModes, humanPlayer;
     RadioButton dtTank, dtSwerve, dtMecanum, dtOmni, dtOther;
     CheckBox highShoot, lowShoot, gearPlace, hasAuto, cheesecake;
+    String[] spinnerAry;
+    Context context;
+    Spinner spnTeamSpinner;
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+        verifyStoragePermissions(this);
+        context = this;
         Button buttonNext = (Button) findViewById(R.id.buttonNext);
         scoutName = (EditText)findViewById(R.id.txtScoutName);
         robotWeight = (EditText)findViewById(R.id.txtWeight);
@@ -41,21 +60,34 @@ public class Questions extends AppCompatActivity {
         gearPlace = (CheckBox)findViewById(R.id.checkGear);
         hasAuto = (CheckBox)findViewById(R.id.checkAuto);
         cheesecake = (CheckBox)findViewById(R.id.checkCheesecake);
+        spnTeamSpinner = (Spinner)findViewById(R.id.spinnerChoseTeam);
+
+        spinnerAry = new String[7];
+        //DummyData
+        spinnerAry[0] = "Pick A Team";
+        spinnerAry[1] = "254";
+        spinnerAry[2] = "976";
+        spinnerAry[3] = "987";
+        spinnerAry[4] = "1114";
+        spinnerAry[5] = "2056";
+        spinnerAry[6] = "2122";
+        //ActualData
+        System.out.println("Hello FakeData");
+        readFile();
+
+        ArrayAdapter<String> teamSpnAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerAry);
+        teamSpnAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnTeamSpinner.setAdapter(teamSpnAdapter);
 
         buttonNext.setOnClickListener(
                 new Button.OnClickListener(){
                     public void onClick(View v)  {
                         saveData();
                         startActivity (new Intent(Questions.this, Comments.class));
-
                     }
                 }
-
-
         );
-
-
-            }
+    }
     private void saveData(){
         SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
@@ -95,6 +127,44 @@ public class Questions extends AppCompatActivity {
         }
         else{
             return "";
+        }
+    }
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    public static void verifyStoragePermissions(Activity activity) {
+        //Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        System.out.println("Hello ReachedVSP");
+        if (permission != PackageManager.PERMISSION_GRANTED){
+            System.out.println("Hello No Permission");
+            //We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+    private void readFile() {
+        try {
+            FileReader fr = new FileReader(Environment.getExternalStorageDirectory() + "/team_names.csv");
+            Scanner scanner = new Scanner(fr);
+            scanner.useDelimiter("\n");
+            ArrayList<String> teams = new ArrayList<>();
+            teams.add("Pick A Team");
+            while (scanner.hasNext()){
+                String team = scanner.next().trim();
+                teams.add(team);
+            }
+            scanner.close();
+            spinnerAry = teams.toArray(new String[teams.size()]);
+        }
+        catch (FileNotFoundException e){
+            Toast t = Toast.makeText(context,
+                    "File not found!", Toast.LENGTH_LONG);
+            t.show();
         }
     }
 }
